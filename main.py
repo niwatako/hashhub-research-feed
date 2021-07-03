23a66f8feae1caab2b311c196b32efc092bb6cff
niwatako <1546218+niwatako@users.noreply.github.com>
2021-07-04 01:31:06 +0900

    Fix pubDate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 import json
 from jinja2 import Environment, FileSystemLoader
@@ -5,8 +6,16 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('./', encoding='utf8'), autoescape = True)
 tmpl = env.get_template('template.xml')
 
-dict = requests.get('https://hashhub-research.com/api/articles?page=1&per=24').json()
-# TODO: 投稿日時をpubData用に変換
-xlm = tmpl.render(dict)
+json = requests.get('https://hashhub-research.com/api/articles?page=1&per=24').json()
+articles = []
+for article in json.get("articles"):
+    articles.append({
+        "title": article.get("title"),
+        "url": "https://hashhub-research.com/articles/" + article.get("slug"),
+        "table_of_contents": article.get("table_of_contents"),
+        "pubDate": datetime.strptime(article.get("created_at"), '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%a, %d %b %Y %H:%M:%S %z')
+    })
+
+xml = tmpl.render({"articles": articles})
 with open('public/feed.xml',mode='w',encoding="utf-8") as f:
-    f.write(str(xlm))
+    f.write(str(xml))
